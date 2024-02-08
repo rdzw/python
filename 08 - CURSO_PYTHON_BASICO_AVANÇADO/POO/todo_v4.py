17409f921fac8f197b9bedcbe72c5d7c8ab98a36
lima <2015002730@ifam.edu.br>
2024-02-08 16:14:42 -0400

    poo

---
+++ 08 - CURSO_PYTHON_BASICO_AVANÇADO/POO/todo_v4.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+
+class Projeto:
+    def __init__(self, nome):
+        self.nome = nome
+        self.tarefas = []
+
+    def __inter__(self):
+        return self.tarefas.__inter__()
+
+    def add(self, descricao):
+        self.tarefas.append(Tarefa(descricao))   
+    
+    def pendentes(self):
+        return [tarefa for tarefa in self.tarefas if not tarefa.feito]
+
+    def procurar(self, descricao):
+        #possivel IndexError
+        return [tarefa for tarefa in self.tarefas
+                if tarefa.descricao == descricao][0]
+    
+    def __str__(self):
+        return f'{self.nome} ({len(self.pendentes())} tarefa(s) pendente(s))'
+
+            
+class Tarefa:
+    def __init__(self, descricao):
+        self.descricao = descricao
+        self.feito = False
+        self.criacao = datetime.now()
+
+    def concluir(self):
+        self.feito = True
+
+    def __str__(self):
+        return self.descricao + ('(concluida)' if self.feito else '')
+    
+
+def main():
+    casa = Projeto('Tarefas de casa')
+    casa.add('Passar roupa')
+    casa.add('lavar prato')
+    print(casa)
+
+    casa.procurar('lavar prato').concluir()
+    for tarefa in casa:
+        print(f'-{tarefa}')
+    print(casa)
+
+    mercado = Projeto('compras no mercado')
+    mercado.add('frutas secas')
+    print(mercado)
+
+    for tarefa in mercado:
+        print(f'-{tarefa}')
+    
+
+if __name__ == '__main__':
+    main()
+        
+        
