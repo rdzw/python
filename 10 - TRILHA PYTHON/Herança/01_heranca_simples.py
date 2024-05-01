87253a47d4ce2af1d38a446abefe3b818297ddde
lima <2015002730@ifam.edu.br>
2024-05-01 17:41:17 -0400

    new

---
+++ 10 - TRILHA PYTHON/Herança/01_heranca_simples.py
@@ -0,0 +1,26 @@
+class Veiculo:
+    def __init__(self,cor,placa,numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_rodas = numero_rodas
+
+    def ligar_motor(self):
+        print("ligar motor")
+
+class Motocicleta(Veiculo):
+    pass
+
+class Carro(Veiculo):
+    pass
+
+class Caminhao(Veiculo):
+    pass
+
+moto = Motocicleta("preta","abc-1234", 2)
+moto.ligar_motor()
+
+carro = Carro("branco", "xde-0098", 4)
+carro.ligar_motor()
+
+caminhao = Caminhao("roxo","gfe-1234",8)
+caminhao.ligar_motor()
