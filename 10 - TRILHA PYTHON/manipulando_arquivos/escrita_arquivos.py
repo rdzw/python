a7e6c3a639fa4e3cc874e83da7dc464765591cf0
lima <2015002730@ifam.edu.br>
2024-05-22 17:12:12 -0400

    new

---
+++ 10 - TRILHA PYTHON/manipulando_arquivos/escrita_arquivos.py
@@ -0,0 +1,4 @@
+file = open("10 - TRILHA PYTHON\\manipulando_arquivos\\teste.txt", "w")
+file.write('Escrevendo dado em outro arquivo ')
+file.writelines('python')
+file.close()
---
+++ 10 - TRILHA PYTHON/manipulando_arquivos/test.py
@@ -0,0 +1,5 @@
+arquivo = open("10 - TRILHA PYTHON\\manipulando_arquivos\\test.txt", "r")
+
+print(arquivo.read())
+
+arquivo.close()
